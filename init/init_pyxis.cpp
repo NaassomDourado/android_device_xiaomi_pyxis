6ae30d578f666a53c058414034053804da1c8dcc
Axmy <mazmee10@gmail.com>
2022-01-22 20:00:08 +0500

    Safetynet update fp

--- init/init_pyxis.cpp
+++ init/init_pyxis.cpp
@@ -55,14 +55,14 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 
 void vendor_load_properties()
 {
-    property_override("ro.build.description","redfin-user 12 SQ1A.220105.002/7961164:user/release-keys");
-    property_override("ro.bootimage.build.fingerprint","google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys");
-    property_override("ro.build.fingerprint","google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys");
-    property_override("ro.odm.build.fingerprint","google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys");
-    property_override("ro.product.build.fingerprint","google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys");
-    property_override("ro.system.build.fingerprint","google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys");
-    property_override("ro.system_ext.build.fingerprint","google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys");
-    property_override("ro.vendor.build.fingerprint","google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys");
+    property_override("ro.build.description","raven-user 12 SQ1D.220105.007 8030436 release-keys");
+    property_override("ro.bootimage.build.fingerprint","google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys");
+    property_override("ro.build.fingerprint","google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys");
+    property_override("ro.odm.build.fingerprint","google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys");
+    property_override("ro.product.build.fingerprint","google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys");
+    property_override("ro.system.build.fingerprint","google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys");
+    property_override("ro.system_ext.build.fingerprint","google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys");
+    property_override("ro.vendor.build.fingerprint","google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys");
     property_override("ro.oem_unlock_supported", "0");
     property_override("ro.boot.verifiedbootstate", "green");
     property_override("ro.boot.flash.locked", "1");
