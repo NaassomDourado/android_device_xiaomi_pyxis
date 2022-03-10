09ad8ef11fab9093372c2196a57f3cdd47f97026
NaassomDourado <naassomdourado@gmail.com>
2022-03-10 16:13:22 -0300

    Blissify

--- bliss_pyxis.mk
+++ bliss_pyxis.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/pyxis/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_pyxis
+PRODUCT_NAME := bliss_pyxis
 PRODUCT_DEVICE := pyxis
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9 Lite
@@ -23,3 +23,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="pyxis"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Official Build
+BLISS_BUILDTYPE=OFFICIAL
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
