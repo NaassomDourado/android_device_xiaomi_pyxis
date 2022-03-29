31e8beea456f22cb603312bacc023b7ac1180960
NaassomDourado <naassomdourado@gmail.com>
2022-03-29 17:43:58 -0300

    Elixir

--- aosp_pyxis.mk
+++ aosp_pyxis.mk
@@ -7,19 +7,15 @@
 $(call inherit-product, device/xiaomi/pyxis/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_pyxis
+PRODUCT_NAME := aosp_pyxis
 PRODUCT_DEVICE := pyxis
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/pyxis/pyxis:11/RKQ1.200826.002/V12.5.2.0.RFCMIXM:user/release-keys"
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="pyxis-user 11 RKQ1.200826.002 V12.5.2.0.RFCMIXM release-keys" \
-    PRODUCT_NAME="pyxis"
+IS_PHONE := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
